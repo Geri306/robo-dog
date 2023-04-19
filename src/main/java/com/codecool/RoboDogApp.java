961dc30022a4a04caa861be1582637412a652502
Gergo Kovacs <gergosmith306@gmail.com>
2023-04-19 16:34:48 +0200

    Rename main class

--- src/main/java/com/codecool/RoboDogApp.java
+++ src/main/java/com/codecool/RoboDogApp.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RoboDog {
+public class RoboDogApp {
     public static void main(String[] args) {
-        SpringApplication.run(RoboDog.class, args);
+        SpringApplication.run(RoboDogApp.class, args);
     }
 }
